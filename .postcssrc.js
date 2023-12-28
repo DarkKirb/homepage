12097f6e6368cddc0db27b2877bcec524e05f3ac
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-12-28 11:46:08 +0100

    add old password blog post

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,24 +3,6 @@ module.exports = {
     "@csstools/postcss-sass": {
       includePaths: ["node_modules"],
     },
-    "postcss-uncss": {
-      html: ["public/**/*.html"],
-      ignore: [
-        "[data-theme=white]",
-        "[data-theme=black]",
-        "[data-theme=sunset]",
-        "[data-theme=light]",
-        '[data-theme="white"]',
-        '[data-theme="black"]',
-        '[data-theme="sunset"]',
-        '[data-theme="light"]',
-        ':root:is([data-theme="white"]):not([data-theme="black"]):not([data-theme="sunset"])',
-      ],
-      htmlroot: "public/",
-      jsdom: {
-        url: "https://lotte.chir.rs/",
-      },
-    },
     cssnano: {
       preset: "advanced",
     },
